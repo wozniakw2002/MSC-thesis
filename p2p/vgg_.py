5942243e1d10f9a1bcf4d15cf0c8922e11bb6c4b
Wiktor <103779975+wozniakw2002@users.noreply.github.com>
2026-04-28 22:52:46 +0200

    p2p

--- p2p/vgg_.py
+++ p2p/vgg_.py
@@ -103,7 +103,7 @@ def _vgg(arch, cfg, batch_norm, pretrained, progress, sync=False, **kwargs):
         kwargs['init_weights'] = False
     model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, sync=sync), **kwargs)
     if pretrained:
-        state_dict = torch.load(model_paths[arch])
+        state_dict = torch.load('/kaggle/input/models/wiktorwoniak/vgg/pytorch/default/1/vgg16_bn-6c64b313.pth')
         model.load_state_dict(state_dict)
     return model
 
